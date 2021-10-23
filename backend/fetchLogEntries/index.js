b634c2e29af9bdc9562b7a66d6809440f2b18d5e
David Hanaford <davehanaford@gmail.com>
2021-10-23 11:31:20 -0400

    code formatting

--- backend/fetchLogEntries/index.js
+++ backend/fetchLogEntries/index.js
@@ -1,60 +1,61 @@
 
 exports.handler = async (event, context, callback) => {
 
-	const mySql = require('mysql');
+    const mySql = require('mysql');
 
-	const connection = mySql.createConnection({
-		host: 'log-entry-1.cwwtcoc9fclh.us-east-1.rds.amazonaws.com',
-		user: 'admin',
-	  	password: 'test1234',
-	  	database: 'log-entry'
-	});
+    // Connect to database
+    const connection = mySql.createConnection({
+        host: 'log-entry-1.cwwtcoc9fclh.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'test1234',
+        database: 'log-entry'
+    });
 
-	console.log(`input: ${JSON.stringify(event)}`);
+    console.log(`input: ${JSON.stringify(event)}`);
 
-	let input = event.queryStringParameters;
+    let input = event.queryStringParameters;
 
-	let sqlSelect = 'SELECT * FROM time_dimension';
-	let values = [];
+    let sqlSelect = 'SELECT * FROM time_dimension';
+    let values = [];
 
-	function generateWhere() {
-		let where = '';
-		let searchString = `%${input.searchString}%`;
-		if (input.searchString) {
-			where = ' where event like ?';
-			values.push(searchString);
-		} else {
-			console.log('seachAll else hit');
-			where = ' WHERE month = ? AND year = ?';
-			values.push(input.month);
-			values.push(input.year);
-		}
-		return where;
-	}
+    function generateWhere() {
+        let where = '';
+        let searchString = `%${input.searchString}%`;
+        if (input.searchString) {
+            where = ' where event like ?';
+            values.push(searchString);
+        } else {
+            console.log('seachAll else hit');
+            where = ' WHERE month = ? AND year = ?';
+            values.push(input.month);
+            values.push(input.year);
+        }
+        return where;
+    }
 
-	let sqlString = sqlSelect + generateWhere();
+    let sqlString = sqlSelect + generateWhere();
 
-	console.log(`sqlString: ${sqlString}`);
+    console.log(`sqlString: ${sqlString}`);
 
-	async function getMonth() {
+    async function getMonth() {
 
-		return new Promise((resolve, reject) => {
-			connection.connect();
+        return new Promise((resolve, reject) => {
+            connection.connect();
 
-	        connection.query(sqlString, values, (error, results) => {
-	            if (error) {
-	                console.log('error retrieving data: ' + error);
-	                reject(error);
-	            } else {
-	            	resolve(results);
-	            }
-	        });
-	       	connection.end();
-	    });
-	}
+            connection.query(sqlString, values, (error, results) => {
+                if (error) {
+                    console.log('error retrieving data: ' + error);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+            connection.end();
+        });
+    }
 
 
-	let results = await getMonth();
+    let results = await getMonth();
 
     let responseBody = {
         result: results
@@ -70,5 +71,6 @@ exports.handler = async (event, context, callback) => {
         "body": JSON.stringify(responseBody),
         "isBase64Encoded": false
     };
+    // Return response for UI
     callback(null, response);
 };
--- backend/saveLogEntry/index.js
+++ backend/saveLogEntry/index.js
@@ -1,46 +1,47 @@
 
 exports.handler = async (event, context, callback) => {
 
-	let mySql = require('mysql');
+    let mySql = require('mysql');
 
-	let connection = mySql.createConnection({
-		host: 'log-entry-1.cwwtcoc9fclh.us-east-1.rds.amazonaws.com',
-		user: 'admin',
-	  	password: 'test1234',
-	  	database: 'log-entry'
-	});
+    // Connect to database
+    let connection = mySql.createConnection({
+        host: 'log-entry-1.cwwtcoc9fclh.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'test1234',
+        database: 'log-entry'
+    });
 
-	console.log('input: ' + JSON.stringify(event));
+    console.log('input: ' + JSON.stringify(event));
 
-	let input = JSON.parse(event.body);
+    let input = JSON.parse(event.body);
 
-	async function saveEntry() {
+    async function saveEntry() {
 
-		return new Promise((resolve, reject) => {
-			connection.connect();
+        return new Promise((resolve, reject) => {
+            connection.connect();
 
-	        connection.query(
-	        	'UPDATE time_dimension set weather = ?, event = ? where id = ?', [input.params.weather, input.params.event, input.params.id], (error, results) => {
-	            if (error) {
-	                console.log('error: ' + error);
-	                reject(error);
+            connection.query('UPDATE time_dimension set weather = ?, event = ? where id = ?', [input.params.weather, input.params.event, input.params.id], (error, results) => {
+                if (error) {
+                    console.log('error: ' + error);
+                    reject(error);
+                } else {
+                    if (results.affectedRows) {
+                        console.log('Entry has been saved');
+                        resolve(results);
+                    } else {
+                        console.log('Entry failed to save');
+                    }
+                }
 
-	            } else {
-	            	if (results.affectedRows) {
-	            		console.log('Entry has been saved');
-	            		resolve(results);
-	                } else {
-	                	console.log('Entry failed to save');
-	                }
-	            }
-	            connection.end();
-	        });
+                connection.end();
 
-	    });
-	}
+            });
+
+        });
+    }
 
 
-	let results = await saveEntry();
+    let results = await saveEntry();
     var responseBody = {
         result: results
     };
@@ -55,5 +56,6 @@ exports.handler = async (event, context, callback) => {
         "body": JSON.stringify(responseBody),
         "isBase64Encoded": false
     };
+    // Return response for UI
     callback(null, response);
 };
