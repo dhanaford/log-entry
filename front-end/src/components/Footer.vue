51f5afe7ddf72ba821fbacedb58a444dd4bf1a0f
David Hanaford <davehanaford@gmail.com>
2021-11-01 12:36:49 -0400

    create footer

---
+++ front-end/src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="footer">
+    Copyright &#169; David Hanaford  {{ currentYear }}   | v.{{ version }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    msg: String,
+  },
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    },
+    version() {
+      return '1.0.0';
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer {
+  background-color: #1e1e1e;
+  color: white;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  padding: 1em;
+}
+</style>
