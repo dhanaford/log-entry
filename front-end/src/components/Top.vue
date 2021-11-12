058e659a46603c207534cc1bcd9cccd4bdf05d46
David Hanaford <davehanaford@gmail.com>
2021-11-11 19:27:04 -0500

    rename todo component to top

--- front-end/src/components/Top.vue
+++ front-end/src/components/Top.vue
@@ -5,7 +5,41 @@
         @click="showNavigation = true">
         <md-icon>menu</md-icon>
       </md-button>
-      <span class="md-title">To Do</span>
+      <md-button @click="handleNav('log')">Log</md-button>
+      <md-button @click="handleNav('news')">News</md-button>
+      <div class="md-toolbar-section-end">
+        <md-menu
+          md-size="big"
+          md-direction="top-start"
+          :md-active.sync="toggleCard"
+        >
+          <md-badge
+            :class="{'hide-notification': !reminders.length}"
+            :md-content="reminders.length"
+            md-dense
+          >
+            <md-button class="md-icon-button" md-menu-trigger>
+              <md-icon>notifications</md-icon>
+            </md-button>
+          </md-badge>
+
+          <md-menu-content>
+            <div class="reminder">
+              <div
+                v-for="(reminder, idx) in reminders"
+                :key="idx" :value="reminder"
+                :class="[{'due-today': formatDate(reminder) === currentDateFormatted}, {'is-past-due': formatDate(reminder) < currentDateFormatted}]"
+              >
+                {{ reminder.reminderName }} - {{ formatDate(reminder).split('-')[1]}} / {{formatDate(reminder).split('-')[2]}}
+                <span v-if="formatDate(reminder) === currentDateFormatted"></span>
+                <span class="remove"
+                  @click="removeReminder(idx)"
+                >X</span>
+              </div>
+            </div>
+          </md-menu-content>
+        </md-menu>
+      </div>
     </md-toolbar>
     <md-drawer
       :md-active.sync="showNavigation" 
@@ -51,26 +85,33 @@
 
 <script>
 import ls from 'local-storage';
+import localStorage from '@/mixins/localStorage.js';
 
 export default {
   name: 'ToDo',
+  mixins: [localStorage],
+  props: ['reminders', 'today'],
   data() {
     return {
       editingItem: {},
       showNavigation: false,
       selected: null,
       idx: 0,
-      toDo: []
+      toDo: [],
+      toggleCard: false,
+      newsSelected: false
     };
-  },
-  computed: {
   },
   mounted() {
     this.getToDos();
   },
+  computed: {
+    currentDateFormatted() {
+      return new Date().toISOString().split('T')[0];
+    }
+  },
   methods: {
     remove(idx) {
-      console.log(idx);
       let indexOfTodoToDelete = this.toDo.findIndex((todoInArray) => todoInArray.idx === idx);
       this.toDo.splice(indexOfTodoToDelete, 1);
       ls.set('log.entry.todo', this.toDo);
@@ -101,6 +142,13 @@ export default {
       if (item) {
         this.editingItem = item;
       }
+    },
+    formatDate(reminder) {
+      reminder = reminder.date.split('T')[0];
+      return reminder;
+    },
+    handleNav(event) {
+      this.$emit('clicked', event);
     }
   }
 };
@@ -108,15 +156,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .md-table table thead {
-  display: none !important;
-} */
 .md-drawer.md-temporary.md-active {
   padding: 1em;
 }
-.remove {
-  float:right;
+.reminder div {
+  height: 30px;
+  border-bottom: 1px solid rgba(255,255,255,0.12);
+  padding-top: 2px;
+}
+.due-today {
+  color: #448aff;
+}
+.is-past-due {
   color: red;
-  font-weight: bold;
+}
+.md-menu-content-container {
+  width: 800px;
 }
 </style>
